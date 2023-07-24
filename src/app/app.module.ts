4919e43c9d74085de8b6af5cfe2f5d36554e3736
Hasantha Karunachandra <32540627+HasanthaKarunachandra@users.noreply.github.com>
2023-07-24 12:21:58 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllComponent } from './components/all/all.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { FindComponent } from './components/find/find.component';
+import { NewComponent } from './components/new/new.component';
+import { UpdateComponent } from './components/update/update.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllComponent,
+    DeleteComponent,
+    FindComponent,
+    NewComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
